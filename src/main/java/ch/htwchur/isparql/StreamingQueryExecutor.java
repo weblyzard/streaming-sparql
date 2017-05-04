ff93adb83fb04d4060591f6194c59b73c682ab36
Albert Weichselbraun <albert@weichselbraun.net>
2017-05-04 09:05:28 +0200

    add: support for content compression and logging.

--- src/main/java/ch/htwchur/isparql/StreamingQueryExecutor.java
+++ src/main/java/ch/htwchur/isparql/StreamingQueryExecutor.java
@@ -6,10 +6,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
@@ -26,10 +26,13 @@ public class StreamingQueryExecutor  {
 	
 	public final static int DEFAULT_BUFFER_SIZE = 1024;
 
-	public final static String USER_AGENT = "iSPARQL Library 0.0.1";
-	public final static String CONTENT_TYPE = "text/tab-separated-values";
+	private final static String USER_AGENT = "iSPARQL Library 0.0.1";
+	private final static String CONTENT_TYPE = "text/tab-separated-values";
+	private final static String COMPRESSED_CONTENT_ENCODING = "gzip";
 	
 	private final static int MAX_GET_QUERY_LEN = 2*1024-1;
+	protected final static Logger log = Logger.getLogger(StreamingQueryExecutor.class.getCanonicalName()); 
+
 	
 	static {
 		System.setProperty("http.maxConnections", Integer.toString(Runtime.getRuntime().availableProcessors()));
@@ -42,28 +45,39 @@ public class StreamingQueryExecutor  {
 	 * @throws IOException
 	 */
 	public static StreamingResultSet getResultSet(String repositoryUrl, String query) throws IOException {
-		URLConnection conn;
+		HttpURLConnection conn;
 		String queryString = URLEncodedUtils.format(Arrays.asList(new Pair("query", query)), StandardCharsets.UTF_8);
 
 		// open connection
 		if ((repositoryUrl.length() + queryString.length()) < MAX_GET_QUERY_LEN) {
-			conn = new URL(repositoryUrl + "?" + queryString).openConnection();
-			conn.setRequestProperty("User-Agent", USER_AGENT);
-			conn.setRequestProperty("Accept", CONTENT_TYPE);
+			conn = (HttpURLConnection) new URL(repositoryUrl + "?" + queryString).openConnection();
+			setCommonHeaders(conn);
 		} else {
 			conn = openPostConnection(repositoryUrl, queryString);
-		}		
+		}
 		
 		// create result set
-		BufferedReader bin = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		log.info(String.format("iSparql receiving '%s' bytes of content type '%s' with encoding '%s'.", 
+				conn.getContentLengthLong(), conn.getContentType(), conn.getContentEncoding()));
+		BufferedReader bin = conn.getContentEncoding().equalsIgnoreCase(COMPRESSED_CONTENT_ENCODING) ?
+				new BufferedReader(new InputStreamReader(new GZIPInputStream(conn.getInputStream()))) :
+					new BufferedReader(new InputStreamReader(conn.getInputStream()));
+					
 		return new StreamingResultSet(bin);
 	}
-	
-	private static URLConnection openPostConnection(String repositoryUrl, String queryString) throws IOException {
+
+	/**
+	 * Handle large queries which require a POST query.
+	 * 
+	 * @param repositoryUrl
+	 * @param queryString
+	 * @return
+	 * @throws IOException
+	 */
+	private static HttpURLConnection openPostConnection(String repositoryUrl, String queryString) throws IOException {
 		URL url = new URL(repositoryUrl);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestProperty("User-Agent", USER_AGENT);
-		conn.setRequestProperty("Accept", CONTENT_TYPE);
+		setCommonHeaders(conn);
 
 		conn.setRequestMethod("POST");
 		conn.setDoOutput(true);
@@ -73,6 +87,16 @@ public class StreamingQueryExecutor  {
 		out.write(queryString.getBytes(Charsets.UTF_8));
 		out.close();
 		return conn;
+	}
+	
+	/**
+	 * Set the HTTP header common to all connections
+	 * @param conn
+	 */
+	private static void setCommonHeaders(HttpURLConnection conn) {
+		conn.setRequestProperty("User-Agent", USER_AGENT);
+		conn.setRequestProperty("Accept", CONTENT_TYPE);
+		conn.setRequestProperty("Accept-Encoding", COMPRESSED_CONTENT_ENCODING);
 	}
 
 	
