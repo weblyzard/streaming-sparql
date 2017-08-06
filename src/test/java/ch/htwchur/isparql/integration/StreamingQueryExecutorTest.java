aace36bbacfe90d80ae83d3aacea35a73321a757
Albert Weichselbraun <albert@weichselbraun.net>
2017-08-06 08:48:46 +0200

    wip: use load to initialize jena data

--- src/test/java/ch/htwchur/isparql/integration/StreamingQueryExecutorTest.java
+++ src/test/java/ch/htwchur/isparql/integration/StreamingQueryExecutorTest.java
@@ -2,6 +2,7 @@ package ch.htwchur.isparql.integration;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,10 @@ import java.util.Map;
 import org.apache.jena.graph.Node;
 import org.junit.ClassRule;
 import org.junit.Test;
+
+import com.spotify.docker.client.DockerClient.ExecCreateParam;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.ExecCreation;
 
 import ch.htwchur.isparql.StreamingQueryExecutor;
 import ch.htwchur.isparql.StreamingResultSet;
@@ -20,19 +25,22 @@ import pl.domzal.junit.docker.rule.WaitFor;
 public class StreamingQueryExecutorTest {
     
     private static final String REPOSITORY_URL = "http://127.0.0.1:3030/test/";
+    private static final String WORK_DIRECTORY = new File(".").getAbsolutePath();
     
     @ClassRule
     public static DockerRule jena = DockerRule.builder()
         .imageName("stain/jena-fuseki")
-        .mountFrom("cafes.ttl.gz").to("/tmp/cafes.ttl.gz")
-        // .cmd("/bin/ls -altr /; ./load.sh test /cafes.ttl.gz; ./fuseki-server")
+        .mountFrom(WORK_DIRECTORY + "/test-datasets").to("/staging")
         .expose("3030", "3030")        
         .waitFor(WaitFor.logMessageSequence("on port 3030"))
         .stopOptions(StopOption.KILL, StopOption.REMOVE)
         .build();
     
     @Test
-    public void integrationTest() throws IOException {
+    public void integrationTest() throws IOException, DockerException, InterruptedException {
+        System.out.println("Working directory: " + WORK_DIRECTORY);
+        // System.out.println(runDockerExecCommand(new String[]{"ls", "-latr",  "/staging"}));
+        System.out.println(runDockerExecCommand(new String[]{"/bin/sh", "./load.sh", "test"}));
         StreamingResultSet s = StreamingQueryExecutor.getResultSet(REPOSITORY_URL, 
                 "SELECT ?s ?p ?o WHERE { ?s ?p ?o. }");
         List<Map<String, Node>> result = new ArrayList<>();
@@ -41,6 +49,16 @@ public class StreamingQueryExecutorTest {
         }
         System.out.println("=====" + result);
         assertEquals(0, result.size());
+    }
+    
+    /**
+     * run a command in the docker container and return the output 
+     */
+    private String runDockerExecCommand(String[] args) throws DockerException, InterruptedException {
+        final ExecCreation cmd = jena.getDockerClient().execCreate(jena.getContainerId(), args,
+                ExecCreateParam.attachStdout(), ExecCreateParam.attachStderr());
+        final String output = jena.getDockerClient().execStart(cmd.id()).readFully();
+        return output;
     }
     
 }
